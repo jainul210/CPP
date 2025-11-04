31a9ce120994196fd274e844c4be0af1f421ac3c
JAINUL ABDEEN <jainulabdeen210@gmail.com>
2025-11-04 23:00:29 +0530

    Initial commit

---
+++ day1.cpp
@@ -0,0 +1,48 @@
+// // Sum of all odd numbers from 1 to n 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter the number : ";
+//     cin >> n;
+//     int sum = 0;
+    
+//     for(unsigned i = 0; i <= n; i++) {
+        
+//         if(i%2 != 0) {
+//             sum += i;
+//         }
+        
+       
+//     }
+//     cout << sum << endl;
+//     return 0;
+// }
+
+
+// // Check if a number is prime or not
+// # include <iostream>
+// using namespace std;
+// int main () {
+//     int n;
+//     cout << "Enter the number";
+//     cin >> n;
+//     int i = 2;
+//     bool isPrime = true;
+//     while(i<n) {
+//         if (n%i==0){
+//             isPrime = false;
+//             break;
+//         }
+//         i+=1;    
+
+//     }
+//     if (isPrime==true){
+
+//             cout << "The given number is Prime number \n";
+//     }else{
+//             cout << "The given number is NOT Prime number \n";
+         
+//         }
+//     return 0;
+// }
