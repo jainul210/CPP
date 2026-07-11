590fd56ec811d01a1b77bd5dbbd56aca73e631cb
JAINUL ABDEEN <jainulabdeen210@gmail.com>
2026-07-11 21:14:02 +0530

    feat: implement program to find the largest number in an array

--- a1.cpp
+++ a1.cpp
@@ -1,1 +1,17 @@
 // write a program to find the largest number from the given array
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[] = {2,5,56,47,84,8,497,43};
+    int length = sizeof(arr) / sizeof(arr[0]);
+   int max_val = arr[0];
+    
+    for (int i = 1; i < length; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+        cout << "The largest number is: " << max_val << endl;
+    
+    return 0;
+}
