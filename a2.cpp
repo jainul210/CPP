cf41abdeffaa980c5a8e2961c0c01aa8fbc29e28
JAINUL ABDEEN <jainulabdeen210@gmail.com>
2026-07-12 23:28:33 +0530

    feat: implement logic to reverse a given number in a2.cpp

--- a2.cpp
+++ a2.cpp
@@ -2,5 +2,12 @@
 # include <iostream>
 using namespace std;
 int main(){
-    
+    int a = 4796;
+    int i = 0;
+    int d = 0;
+    while (i==0){
+        int c = a%10;
+        a = a/10;
+
+    }
 }
